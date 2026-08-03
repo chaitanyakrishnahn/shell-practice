3c12554dcfff14e28ce576fed57131c0e550b8a1
chaitanya <chaitanyakrishnahn@gmail.com>
2026-08-03 16:36:18 +0530

    Added conversation script

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,3 +3,8 @@ echo "please enter the username::"
 read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variable
 
 echo "Username is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD
+
+echo "Password is: $PASSWORD"
