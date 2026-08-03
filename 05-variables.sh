b927929ecff1bb3bf756ba3b53bfb103300b86c2
chaitanya <chaitanyakrishnahn@gmail.com>
2026-08-03 18:06:26 +0530

    Added conversation script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 TIMESTAMP=$(date)
-
 echo "Time is: $TIMESTAMP"
+
+START_TIME=$(date +%S)
+
+sleep 10
+
+END_TIME=$(date +%S)
+
+TOTAL_TIME=$((SEND_TIME-$START_TIME))
+
+echo "Script executed in $TOTAL_TIME seconds"
 
